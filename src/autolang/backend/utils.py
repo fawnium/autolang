e9d71d86988788f9b865220acb13a963b6cfbb26
Lyons / fawnium <oisinlyons1@gmail.com>
2025-10-10 19:05:39 +0100

    rewrote word-generating funcs in utils.py to optimise memory use

--- src/autolang/backend/utils.py
+++ src/autolang/backend/utils.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from collections.abc import Iterable
+from collections.abc import Iterable, Generator
 import re
 
 '''
@@ -7,48 +7,72 @@ General utility functions for autolang
 - Primarily generating words over alphabets
 '''
 
-# Return tuple of all words of length exactly `n` over given `alphabet`
-@lru_cache(maxsize=None)
-def _words_of_length(n: int, alphabet: tuple[str, ...]) -> tuple[str, ...]:
+# Maximum size of tuple of generated words before raising error to prevent memory overflow
+def get_max_words_size() -> int:
+    return int(1e6) # Default 1 million - should be ok on most systems
+    # TODO ? use `sys` for better estimate?
+
+# Number of words of length exactly n for given number of alphabet letters
+def get_alphabet_size_single_length(n: int, len_alphabet: int) -> int:
+    pass # TODO
+
+# Number of words of length less than or equal to n for given alphabet size
+def get_alphabet_size_all_lengths(n: int, len_alphabet: int) -> int:
+    pass # TODO
+
+
+# Return generator for all words of length exactly n over given alphabet
+def _words_of_length_gen(n: int, alphabet: tuple[str, ...]) -> Generator[str]:
     '''
-    - create list of words of length n, using list of words of length n-1
-    - if n is zero, simply return empty word list
-    - if n is nonzero, for each word of length n-1 and for each letter in alphabet, concatenate word and letter to make word of 
-      length n
-    - add new word to list
+    - recursively generate all words of length n, by appending each alphabet letter to each word length n - 1
+    - use `yield` to save memory with lazy evaluation
     '''
     if n < 0:
         raise ValueError('Argument \'n\' must be non-negative.')
     # Base case
     if n == 0:
-        return ('',)
+        yield ''
     # Recursive case
-    return tuple(word + letter for word in _words_of_length(n - 1, alphabet) for letter in alphabet)
+    else:
+        for word in _words_of_length_gen(n - 1, alphabet):
+            for letter in alphabet:
+                yield word + letter
 
 # Public wrapper for above
-def words_of_length(n: int, alphabet: Iterable[str]) -> tuple[str, ...]:
+def words_of_length(n: int, alphabet: Iterable[str], lazy = True) -> Generator[str] | tuple[str, ...]:
     '''
-    - ensure alphabet is hashable before entering recursion
+    - convert alphabet to tuple before generating alphabet
+    - in particular ensure hashable for lru cache
+        - NOTE lru cache no longer used, but ensure hashability just in case
     '''
     if n < 0:
         raise ValueError('Argument \'n\' must be non-negative.')
-    return _words_of_length(n, tuple(alphabet))
+    words = _words_of_length_gen(n, tuple(alphabet)) # Generator for words of length n
+    return words if lazy else tuple(words)
 
-# Return tuple of all words of length less than or equal to `n` over given `alphabet`
-def words_to_length(n: int, alphabet: Iterable[str]) -> tuple[str, ...]:
+
+def words_to_length(n: int, alphabet: Iterable[str], lazy = True) -> Generator[str] | tuple[str, ...]:
     '''
-    - for all 0 <= i <= n, add words of length i to result, then return
+    for 0 <= i <= n:
+        - yield each word of length i
+        - this covers all words of all lengths up to n
+    - return generator, or convert to tuple if lazy = False
     '''
     if n < 0:
         raise ValueError('Argument \'n\' must be non-negative.')
-    words = []
-    for i in range(n + 1):
-        words += words_of_length(i, alphabet)
-    return tuple(words)
+    # Generator for words of length n or less
+    def _gen():
+        for i in range(n + 1):
+            for word in words_of_length(i, alphabet, lazy = True):
+                yield word
+    return _gen() if lazy else tuple(_gen())
+
+
+
 
 # Return all words up to given length that match given regular expression
-def words_to_length_from_regex(n: int, alphabet: Iterable, regex: str) -> tuple[str, ...]:
+def words_to_length_from_regex(n: int, alphabet: Iterable[str], regex: str) -> tuple[str, ...]:
     # Convert formal regex to python-regex
     py_regex = regex.replace('.', '') # Remove explicit concat if present
     py_regex = py_regex.replace('+', '|') # Union has a different representation in python regex
-    return tuple(word for word in words_to_length(n, alphabet) if re.fullmatch(py_regex, word))
+    return tuple(word for word in words_to_length(n, alphabet, lazy = True) if re.fullmatch(py_regex, word))
