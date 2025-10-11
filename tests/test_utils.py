00594af84f86469e1a09665eb82ee16e0c2978b0
Lyons / fawnium <oisinlyons1@gmail.com>
2025-10-11 12:29:31 +0100

    Updated utils tests for new function behaviour

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -4,7 +4,7 @@ from autolang.backend.utils import words_of_length, words_to_length
 class TestWordsOfLength(unittest.TestCase):
 
     def test_length_zero(self):
-        self.assertEqual(words_of_length(0, 'ab'), ('',))
+        self.assertEqual(words_of_length(0, 'ab', lazy = False), ('',))
 
     def test_length_one(self):
         self.assertEqual(set(words_of_length(1, 'ab')), {'a', 'b'})
@@ -13,7 +13,7 @@ class TestWordsOfLength(unittest.TestCase):
         self.assertEqual(set(words_of_length(2, 'ab')), {'aa', 'ab', 'ba', 'bb'})
 
     def test_length_twelve(self):
-        self.assertEqual(len(words_of_length(12, 'ab')), 2 ** 12)
+        self.assertEqual(len(words_of_length(12, 'ab', lazy = False)), 2 ** 12)
 
     def test_from_list(self):
         self.assertEqual(set(words_of_length(1, ['a', 'b'])), {'a', 'b'})
@@ -23,13 +23,13 @@ class TestWordsOfLength(unittest.TestCase):
             words_of_length(-1, 'ab')
 
     def test_empty_alphabet(self):
-        self.assertEqual(words_of_length(1, ''), tuple())
+        self.assertEqual(words_of_length(1, '', lazy = False), tuple())
 
 
 class TestWordsToLength(unittest.TestCase):
 
     def test_length_zero(self):
-        self.assertEqual(words_to_length(0, 'ab'), ('',))
+        self.assertEqual(words_to_length(0, 'ab', lazy = False), ('',))
 
     def test_length_one(self):
         self.assertEqual(set(words_to_length(1, 'ab')), {'', 'a', 'b'})
@@ -45,7 +45,7 @@ class TestWordsToLength(unittest.TestCase):
             words_to_length(-1, 'ab')
 
     def test_empty_alphabet(self):
-        self.assertEqual(words_to_length(1, ''), ('',))
+        self.assertEqual(words_to_length(1, '', lazy = False), ('',))
 
 
 class TestWordsToLengthFromRegex(unittest.TestCase):
