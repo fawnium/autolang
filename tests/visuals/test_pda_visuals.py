6b34746938ccc404d663b22c70daa20927d224e5
Lyons / fawnium <oisinlyons1@gmail.com>
2025-11-09 19:20:27 +0000

    Add test cases for _get_pda_digraph()

--- tests/visuals/test_pda_visuals.py
+++ tests/visuals/test_pda_visuals.py
@@ -1,9 +1,12 @@
 import unittest
+import networkx as nx
+
 from autolang.visuals.pda_visuals import (config_to_str,
                                           next_configs_to_str,
                                           _transition_table_pda,
                                           _get_pda_digraph)
-from autolang.visuals.magic_chars import EPSILON
+from autolang.visuals.magic_chars import EPSILON, RIGHT_ARROW
+from autolang.visuals.settings_visuals import DEFAULT_ACCEPT_COL, DEFAULT_REJECT_COL
 from setup_automata import pda1
 
 class TestConfigToStr(unittest.TestCase):
@@ -57,4 +60,30 @@ class TestTransitionTablePDA(unittest.TestCase):
 
 class TestGetPDADigraph(unittest.TestCase):
 
-    pass
+    def test_pda_1(self):
+        digraph = _get_pda_digraph(pda1.transition, pda1.start, pda1.accept, filename='test')
+
+        # States and edges
+        self.assertEqual(set(digraph.nodes), set(pda1.states))
+        self.assertEqual(set(digraph.edges), {('q1','q2'), ('q2','q2'), ('q2','q3'), ('q3','q3'), ('q3','q4')})
+
+        # Node colors
+        self.assertEqual(digraph.nodes['q1']['color'], DEFAULT_ACCEPT_COL)
+        self.assertEqual(digraph.nodes['q2']['color'], DEFAULT_REJECT_COL)
+        self.assertEqual(digraph.nodes['q3']['color'], DEFAULT_REJECT_COL)
+        self.assertEqual(digraph.nodes['q4']['color'], DEFAULT_ACCEPT_COL)
+
+        # Edge labels
+        edge_labels = nx.get_edge_attributes(digraph, 'label')
+        self.assertEqual(edge_labels[('q1','q2')], EPSILON + ',' + EPSILON + RIGHT_ARROW + '$')
+        self.assertEqual(edge_labels[('q2','q2')], '0,' + EPSILON + RIGHT_ARROW + '0')
+        self.assertEqual(edge_labels[('q2','q3')], '1,0' + RIGHT_ARROW + EPSILON)
+        self.assertEqual(edge_labels[('q3','q3')], '1,0' + RIGHT_ARROW + EPSILON)
+        self.assertEqual(edge_labels[('q3','q4')], EPSILON + ',$' + RIGHT_ARROW + EPSILON)
+
+        # Metadata
+        self.assertEqual(digraph.graph['start'], 'q1')
+        self.assertEqual(set(digraph.graph['accept']), set(('q1','q4')))
+        self.assertEqual(digraph.graph['title'], 'PDA with 4 states, input alphabet {0,1}, stack {$,0}')
+        self.assertEqual(digraph.graph['kind'], 'PDA')
+        self.assertEqual(digraph.graph['filename'], 'test')
